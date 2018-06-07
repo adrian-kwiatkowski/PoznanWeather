488626a2d5ffd1648a46b75bbf0d6e30bba3c4e1
Adrian Kwiatkowski <kwiatkowski.adrian@icloud.com>
2018-06-07 08:19:54 +0200

    final commit

---
+++ Pods/Target Support Files/Pods-PoznanWeather/Pods-PoznanWeather-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PoznanWeather : NSObject
+@end
+@implementation PodsDummy_Pods_PoznanWeather
+@end
---
+++ Pods/Target Support Files/Pods-PoznanWeatherTests/Pods-PoznanWeatherTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PoznanWeatherTests : NSObject
+@end
+@implementation PodsDummy_Pods_PoznanWeatherTests
+@end
